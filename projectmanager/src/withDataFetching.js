5559b78ee1c4f06d35c5d6db8d0960da8f96ed1f
Treek <nbatre1@yahoo.com>
2020-01-03 13:55:26 -0600

    keep moving forward

---
+++ projectmanager/src/withDataFetching.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+// creating an highorder component that returns a  new component
+
+export default function withDataFetching(wrappedComponent){
+
+
+  return class extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+         data:[],
+         loading:true,
+         error:''
+        }
+    }
+
+
+    async componentDidMount(){
+
+       try {
+           const data = await fetch(this.props.datasource)
+
+           const dataJSON = await data.json();
+
+
+           if(dataJSON){
+               this.setState({
+                   data:dataJSON,
+                   loading:false
+               });
+           }
+       }
+
+        catch(error){
+            this.setState({
+                loading:false,
+                error:error.message
+            })
+        }
+
+
+
+
+
+
+
+
+    }
+      
+  }
+}
